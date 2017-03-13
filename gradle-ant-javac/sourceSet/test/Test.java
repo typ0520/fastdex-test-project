8367a07433e5abde086f17ad2585e6f30a88b9fb
佟亚鹏 <yapeng.tong@baidao.comc>
2017-03-13 23:54:28 +0800

    gradle-ant-javac

---
+++ gradle-ant-javac/sourceSet/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import android.widget.Toast;
+
+public class Test {
+	public Test() {
+		new test2.Test2();
+		Toast.makeText(null,null,0);
+	}
+}
---
+++ gradle-ant-javac/sourceSet/test/Test3.java
@@ -0,0 +1,11 @@
+package test;
+
+import android.widget.Toast;
+
+public class Test3 {
+	public Test3() {
+		new test2.Test2();
+		Toast.makeText(null,null,0);
+
+	}
+}
