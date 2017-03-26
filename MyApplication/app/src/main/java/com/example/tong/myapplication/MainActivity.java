e9778201d2b42f448d0e4224fd626943db50853c
佟亚鹏 <yapeng.tong@baidao.comc>
2017-03-26 23:10:32 +0800

    jarmerge

---
+++ MyApplication/app/src/main/java/com/example/tong/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tong.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"114440001",Toast.LENGTH_LONG).show();
+    }
+}
