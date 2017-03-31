ef597bbcfd1f7a4ebe042d164040f07b743aedd3
typ0520 <yapeng.tong@baidao.comc>
2017-03-31 17:02:00 +0800

    x

--- file-diff/src/test/java/SourceSetSnapshootTest.java
+++ file-diff/src/test/java/SourceSetSnapshootTest.java
@@ -1,9 +1,13 @@
+import com.dx168.fastdex.build.snapshoot.api.Snapshoot;
+import com.dx168.fastdex.build.snapshoot.api.Status;
 import com.dx168.fastdex.build.snapshoot.sourceset.SourceSetResultSet;
 import com.dx168.fastdex.build.snapshoot.sourceset.SourceSetSnapshoot;
 import junit.framework.TestCase;
 import org.junit.Test;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 
 /**
  * Created by tong on 17/3/31.
@@ -75,6 +79,53 @@ public class SourceSetSnapshootTest extends TestCase {
 
         SourceSetResultSet sourceSetResultSet = (SourceSetResultSet) now.diff(old);
         sourceSetResultSet.serializeTo(new FileOutputStream(new File(workDir,"diff.json")));
+    }
+
+    @Test
+    public void testDiff2() throws Throwable {
+        if (!isDir(source_set1) || !isDir(source_set2) || !isDir(source_set11) || !isDir(source_set22)) {
+            System.err.println("Test-env not init!!");
+        }
+
+        SourceSetSnapshoot now = new SourceSetSnapshoot(new File(workDir),source_set1);
+        SourceSetSnapshoot old = new SourceSetSnapshoot(new File(workDir),source_set1);
+
+        now.serializeTo(new FileOutputStream(new File(workDir,"now.json")));
+
+        SourceSetResultSet sourceSetResultSet = (SourceSetResultSet) now.diff(old);
+        SourceSetResultSet sourceSetResultSet2 = (SourceSetResultSet) now.diff(old);
+        assertTrue(sourceSetResultSet.equals(sourceSetResultSet2));
+
+        SourceSetSnapshoot now1 = (SourceSetSnapshoot) Snapshoot.load(new FileInputStream(new File(workDir,"now.json")),SourceSetSnapshoot.class);
+        SourceSetResultSet sourceSetResultSet3 = (SourceSetResultSet) now1.diff(old);
+        assertTrue(sourceSetResultSet.equals(sourceSetResultSet3));
+
+
+
+        //now.serializeTo(new FileOutputStream(new File(workDir,"now.json")));
+
+        //SourceSetSnapshoot old = new SourceSetSnapshoot(new File(workDir),source_set11);
+
+//        SourceSetResultSet sourceSetResultSet = (SourceSetResultSet) now.diff(old);
+//        sourceSetResultSet.serializeTo(new FileOutputStream(new File(workDir,"diff.json")));
+    }
+
+    @Test
+    public void testDiff3() throws Throwable {
+        if (!isDir(source_set1) || !isDir(source_set2) || !isDir(source_set11) || !isDir(source_set22)) {
+            System.err.println("Test-env not init!!");
+        }
+
+        SourceSetSnapshoot now = new SourceSetSnapshoot(new File(workDir),source_set1);
+        SourceSetSnapshoot old = new SourceSetSnapshoot(new File(workDir),source_set1);
+
+        SourceSetResultSet sourceSetResultSet = (SourceSetResultSet) now.diff(old);
+        SourceSetResultSet sourceSetResultSet2 = (SourceSetResultSet) now.diff(old);
+        assertTrue(sourceSetResultSet.equals(sourceSetResultSet2));
+
+        SourceSetSnapshoot now1 = (SourceSetSnapshoot) Snapshoot.load(new FileInputStream(new File(workDir,"now.json")),SourceSetSnapshoot.class);
+        SourceSetResultSet sourceSetResultSet3 = (SourceSetResultSet) now1.diff(old);
+        assertTrue(sourceSetResultSet.equals(sourceSetResultSet3));
     }
 
     public boolean isDir(File dir) {
