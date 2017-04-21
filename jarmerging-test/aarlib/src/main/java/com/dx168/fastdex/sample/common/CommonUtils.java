eb34300ac2c8f167be4feab10992428359938feb
typ0520 <yapeng.tong@baidao.comc>
2017-04-21 18:25:19 +0800

    x

---
+++ jarmerging-test/aarlib/src/main/java/com/dx168/fastdex/sample/common/CommonUtils.java
@@ -0,0 +1,18 @@
+package com.dx168.fastdex.sample.common;
+
+/**
+ * Created by tong on 17/4/13.
+ */
+public class CommonUtils {
+    public static String str = CommonUtils.class.getSimpleName() + ".str1";
+
+    public CommonUtils() {
+
+
+        new C3XXXX();
+    }
+
+    public static class C3XXXX {
+
+    }
+}
---
+++ jarmerging-test/javalib/src/main/java/com/dx168/fastdex/sample/javalib/JavaLib.java
@@ -0,0 +1,12 @@
+package com.dx168.fastdex.sample.javalib;
+
+/**
+ * Created by tong on 17/4/14.
+ */
+public class JavaLib {
+    public static String str = JavaLib.class.getSimpleName() + ".str";
+
+    public JavaLib() {
+
+    }
+}
