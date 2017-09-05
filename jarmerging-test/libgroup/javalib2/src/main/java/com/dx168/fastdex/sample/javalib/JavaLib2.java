22e611b958e6f8f4ae989fa23d63165ecd4397ee
typ0520 <yapeng.tong@baidao.comc>
2017-09-05 14:55:15 +0800

    resolveProjectDependency

---
+++ jarmerging-test/libgroup/javalib2/src/main/java/com/dx168/fastdex/sample/javalib/JavaLib2.java
@@ -0,0 +1,12 @@
+package com.dx168.fastdex.sample.javalib;
+
+/**
+ * Created by tong on 17/4/14.
+ */
+public class JavaLib2 {
+    public static String str = JavaLib2.class.getSimpleName() + ".str";
+
+    public JavaLib2() {
+
+    }
+}
