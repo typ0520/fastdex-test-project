3278fcbc4d9ab9c4d6eaad5967fb64dc9f1b76e2
typ0520 <yapeng.tong@baidao.comc>
2017-09-20 13:22:20 +0800

    build-cache-test

---
+++ build-cache-test/app/src/main/java/com/example/tong/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tong.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"567575",Toast.LENGTH_LONG).show();
+    }
+}
---
+++ build-cache-test/app/src/main/java/com/example/tong/myapplication/SampleApplication.java
@@ -0,0 +1,10 @@
+package com.example.tong.myapplication;
+
+import android.app.Application;
+
+/**
+ * Created by tong on 17/3/18.
+ */
+
+public class SampleApplication extends Application {
+}
