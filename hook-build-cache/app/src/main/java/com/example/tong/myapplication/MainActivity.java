cf69ee653ef58a4a17974528f9be2391ba9de0d2
typ0520 <yapeng.tong@baidao.comc>
2017-09-27 14:43:17 +0800

    x

--- hook-build-cache/app/src/main/java/com/example/tong/myapplication/MainActivity.java
+++ hook-build-cache/app/src/main/java/com/example/tong/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tong.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -11,6 +12,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"m1" ,Toast.LENGTH_LONG).show();
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        tv1.setText("m2");
+        //Toast.makeText(this,"m2" ,Toast.LENGTH_LONG).show();
     }
 }
