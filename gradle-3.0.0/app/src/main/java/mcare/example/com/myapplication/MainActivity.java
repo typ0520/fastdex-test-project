c1f441b857a723b0b29d2d0e1e32daa2e6e9eb95
typ0520 <yapeng.tong@baidao.comc>
2017-11-02 16:15:52 +0800

    gradle 3.0.0

--- gradle-3.0.0/app/src/main/java/mcare/example/com/myapplication/MainActivity.java
+++ gradle-3.0.0/app/src/main/java/mcare/example/com/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"11",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"2",Toast.LENGTH_LONG).show();
     }
 }
---
+++ gradle-3.0.0/javalib/src/debug/java/mcare/example/com/javalib/JavaLib_Debug.java
@@ -0,0 +1,4 @@
+package mcare.example.com.javalib;
+
+public class JavaLib_Debug {
+}
